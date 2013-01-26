4182d99983d77b05259741d141dc55638e0e12b8
Marsel Zaripov <marszaripov@gmail.com>
2013-01-26 22:16:57 +0400

    add headers of write functions. add new macros

--- deflate.h
+++ deflate.h
@@ -7,14 +7,20 @@
 #define OUTPUT "output"
 #define N 8
 #define GetBit(reg, bit)   (reg & (1 << bit) ? 1 : 0)
-#define SetBit(reg, bit)   (reg |= (1<<bit))
-#define ClearBit(reg, bit) (reg &= (~(1<<bit)))
+#define SetBit(reg, bit)   (reg |= (1 << bit))
+#define ClearBit(reg, bit) (reg &= (~(1 << bit)))
+#define BitIsSet(reg, bit) ((reg & (1 << bit)) != 0)
 #define DICT_SIZE_Q 32769 /*32768 + 1*/
 #define LEN_SIZE_Q 259 /*258 + 1*/
+#define DICT_MAX 32768
+#define LEN_MAX 258
+#define EL_SIZE 1
 
 typedef unsigned char byte;
 void die(char *);
 void print_bytes(int, size_t);
 void static_deflate(FILE *, FILE *);
+void write_literal(FILE *, byte);
+void write_pointer(FILE *, size_t, size_t);
 
 #endif
