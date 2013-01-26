4e319c3d1dde5614d728e18fb4113983113dc85b
Ruslan <pycz2007@yandex.ru>
2013-01-26 19:56:48 +0400

    Just secure against future macro problems
    Так, мало ли.

--- deflate.h
+++ deflate.h
@@ -6,9 +6,9 @@
 #define INPUT "input"
 #define OUTPUT "output"
 #define N 8
-#define GetBit(reg, bit)   (reg & (1 << bit) ? 1 : 0)
-#define SetBit(reg, bit)   (reg |= (1<<bit))
-#define ClearBit(reg, bit) (reg &= (~(1<<bit)))
+#define GetBit(reg, bit)   ((reg) & (1 << (bit)) ? 1 : 0)
+#define SetBit(reg, bit)   ((reg) |= (1<<(bit)))
+#define ClearBit(reg, bit) ((reg) &= (~(1<<(bit))))
 #define DICT_SIZE_Q 32769 /*32768 + 1*/
 #define LEN_SIZE_Q 259 /*258 + 1*/
 
