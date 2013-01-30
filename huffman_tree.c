c5302115fc2e63a1dd0676806641787f1f7f8e08
Marsel Zaripov <marszaripov@gmail.com>
2013-01-31 02:15:21 +0400

    add counting of huffman codes len, printf huffman tree

--- huffman_tree.c
+++ huffman_tree.c
@@ -1,9 +1,46 @@
 #include "huffman_tree.h"
 
 
+huffman_tree *new_huffman_tree(prob_t prob, 
+							   two_bytes code, 
+							   huffman_tree *left, 
+							   huffman_tree *right)
+{
+	huffman_tree *tree = (huffman_tree *) malloc(sizeof(huffman_tree));
+	tree->probability = prob;
+	tree->code = code;
+	tree->len = 0;
+	tree->left = left;
+	tree->right = right;
+	return tree;
+}
+
+void count_len_huffman_tree(huffman_tree *tree, size_t len)
+{
+	if (tree->left == NULL && tree->right == NULL) {
+		if (len == 0)
+			len++;
+		tree->len = len;
+	} else {
+		len++;
+		count_len_huffman_tree(tree->left, len);
+		count_len_huffman_tree(tree->right, len);
+	}
+}
 
 int compare_huffman_tree(const void *tree1, const void *tree2)
 {
-	return ((huffman_tree *) tree2)->probability - 
-		((huffman_tree *) tree1)->probability;
+	return (*((huffman_tree **) tree2))->probability - 
+		(*((huffman_tree **) tree1))->probability;
+}
+
+void print_huffman_tree(huffman_tree *tree)
+{
+	if (tree->left == NULL)
+		printf("code = %d, len = %d; ", tree->code, tree->len);
+	else {
+		print_huffman_tree(tree->left);
+		printf("code = %d, len = %d; ", tree->code, tree->len);
+		print_huffman_tree(tree->right);
+	}
 }
--- huffman_tree.h
+++ huffman_tree.h
@@ -2,13 +2,22 @@
 #define HUFFMAN_TREE_H
 #include "deflate.h"
 
+typedef size_t prob_t;
 typedef struct _huffman_tree {
-	size_t probability;
+	prob_t probability;
+	two_bytes code;
+	size_t len;
 	struct _huffman_tree *left;
 	struct _huffman_tree *right;
-	two_bytes code;
 } huffman_tree;
 
+huffman_tree *new_huffman_tree(prob_t, 
+							   two_bytes, 
+							   huffman_tree *, 
+							   huffman_tree *);
+
+void count_len_huffman_tree(huffman_tree *, size_t);
 int compare_huffman_tree(const void *, const void *);
+void print_huffman_tree(huffman_tree *);
 
 #endif
