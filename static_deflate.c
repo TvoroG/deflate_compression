a7f6e436456940ffd2a8b58e757d39d6bc1f9060
Marsel Zaripov <marszaripov@gmail.com>
2013-01-30 10:01:55 +0400

    remove alphabets from this files

--- static_deflate.c
+++ static_deflate.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include "static_deflate.h"
 #include "deflate.h"
+#include "alphabets.h"
 #include "cyclic_queue.h"
 
 void static_deflate(bool isfinal)
@@ -95,7 +96,7 @@ static void write_huffman_code(two_bytes huff_code, size_t num)
 	}
 }
 
-static void write_bits(byte bits, size_t bits_num)
+static void write_bits(two_bytes bits, size_t bits_num)
 {
 	int i;
 	for (i = 0; i < bits_num; i++) {
@@ -139,53 +140,3 @@ static void get_huffman_code_of_litlen(two_bytes literal,
 		literal - huffman_codes[i].base_lit_value;
 	*code_len = huffman_codes[i].bits_num;
 }
-
-static two_bytes get_code_of_length(size_t length)
-{
-	assert(length >= 3 && length <= 258);
-	bool found = false;
-	int i;
-	two_bytes code = LEN_CODE_BEGINNING;
-	for (i = 0; i < LEN_CODE_NUM - 1 && !found; i++, code++) {
-		if (length >= length_codes[i].base_len &&
-			length <= length_codes[i + 1].base_len - 1)
-			found = true;
-	}
-	/* if not found then return last one */
-	return found ? code - 1 : LEN_CODE_BEGINNING + LEN_CODE_NUM - 1;
-}
-
-static void get_extra_bits_of_length(size_t length,
-							  two_bytes len_code,
-							  byte *extra_bits, 
-							  size_t *bits_num)
-{
-	int i = len_code - LEN_CODE_BEGINNING;
-	*bits_num = length_codes[i].extra_bits_num;
-
-	*extra_bits = length - length_codes[i].base_len;
-}
-
-static byte get_code_of_offset(size_t offset)
-{
-	assert(offset >= 1 && offset <= 32768);
-	byte i;
-	for (i = 0; i < OFF_CODE_NUM - 1; i++) {
-		if (offset >= offset_codes[i].base_off &&
-			offset <= offset_codes[i + 1].base_off - 1)
-			break;
-	}
-	return i;
-}
-
-static void get_extra_bits_of_offset(size_t offset, 
-							  byte off_code, 
-							  two_bytes *extra_bits, 
-							  size_t *bits_num)
-{
-	*bits_num = offset_codes[off_code].extra_bits_num;
-	*extra_bits = offset - offset_codes[off_code].base_off;
-}
-
-
-
--- static_deflate.h
+++ static_deflate.h
@@ -18,81 +18,6 @@ static struct {
 	8, 280, 192
 };
 
-#define LEN_CODE_BEGINNING 257
-#define LEN_CODE_NUM 29 /* size of length_codes array */
-static struct {
-	size_t extra_bits_num;
-	size_t base_len;
-} length_codes[] = {
-	0, 3,
-	0, 4,
-	0, 5,
-	0, 6,
-	0, 7,
-	0, 8,
-	0, 9,
-	0, 10,
-	1, 11,
-	1, 13,
-	1, 15,
-	1, 17,
-	2, 19,
-	2, 23,
-	2, 27,
-	2, 31,
-	3, 35,
-	3, 43,
-	3, 51,
-	3, 59,
-	4, 67,
-	4, 83,
-	4, 99,
-	4, 115,
-	5, 131,
-	5, 163,
-	5, 195,
-	5, 227,
-	0, 258
-};
-
-#define OFF_CODE_NUM 30 /* size of offset_codes array */
-#define OFF_CODE_LEN 5 /* num of bits in offset code is fixed */
-static struct {
-	size_t extra_bits_num;
-	size_t base_off;
-} offset_codes[] = {
-	0, 1,
-	0, 2,
-	0, 3,
-	0, 4,
-	1, 5,
-	1, 7,
-	2, 9,
-	2, 13,
-	3, 17,
-	3, 25,
-	4, 33,
-	4, 49,
-	5, 65,
-	5, 97,
-	6, 129,
-	6, 193,
-	7, 257,
-	7, 385,
-	8, 513,
-	8, 769,
-	9, 1025,
-	9, 1537,
-	10, 2049,
-	10, 3073,
-	11, 4097,
-	11, 6145,
-	12, 8193,
-	12, 12289,
-	13, 16385,
-	13, 24577
-};
-
 extern FILE *input;
 extern FILE *output;
 extern byte write_b;
@@ -106,22 +31,12 @@ static void write_literal(two_bytes);
 static void write_pointer(size_t, size_t);
 static void write_static_header(bool);
 static void write_huffman_code(two_bytes, size_t);
-static void write_bits(byte, size_t);
+static void write_bits(two_bytes, size_t);
 static void next_bit();
 static void byte_flush();
 
 static void get_huffman_code_of_litlen(two_bytes, 
 								two_bytes *, 
 								size_t *);
-static two_bytes get_code_of_length(size_t);
-static void get_extra_bits_of_length(size_t, 
-									 two_bytes, 
-									 byte *, 
-									 size_t *);
-static byte get_code_of_offset(size_t);
-static void get_extra_bits_of_offset(size_t, 
-									 byte, 
-									 two_bytes *, 
-									 size_t *);
 
 #endif
