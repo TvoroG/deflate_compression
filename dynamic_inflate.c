b753c6cb65ba27c68addff7f6ba28d41845e4a14
Marsel Zaripov <marszaripov@gmail.com>
2013-03-18 00:50:17 +0400

    build tree

--- dynamic_inflate.c
+++ dynamic_inflate.c
@@ -2,6 +2,8 @@
 
 #include <string.h>
 
+#include "alphabets.h"
+
 /*void build_huffman_codes(huffman_code huff_code[], size_t size);*/
 void count_number_of_codes(size_t bl_count[], 
 						   huffman_code huff_code[],
@@ -13,20 +15,29 @@ void assign_numerical_values_to_all_codes(size_t next_code[],
 										  huffman_code huff_code[], 
 										  size_t size);
 size_t get_max_bits(size_t bl_count[], size_t bl_count_size);
+void print_massive(size_t mas[], size_t size);
 /*void print_huffman_codes(huffman_code huff_code[], size_t size);*/
 
 
 void dynamic_inflate(reader_t *reader)
 {
-	byte hlit = read_HLIT(reader) - 257;
-	byte hdist = read_HDIST(reader) - 1;
-	byte hclen = read_HCLEN(reader) - 4;
+	size_t hlit = read_HLIT(reader) + 257;
+	size_t hdist = read_HDIST(reader) + 1;
+	size_t hclen = read_HCLEN(reader) + 4;
 
-	huffman_code cl_for_cl[hclen];
+	size_t cl_len = CODE_LENGTH_ORDER_SIZE;
+	huffman_code cl_for_cl[cl_len];
+	memset(cl_for_cl, 0, sizeof(huffman_code) * cl_len);
 	read_code_length_for_code_length(reader, cl_for_cl, hclen);
-	build_huffman_codes(cl_for_cl, hclen);
-	
-
+	build_huffman_codes(cl_for_cl, cl_len);
+	print_huffman_codes(cl_for_cl, cl_len);
+	/*
+	huffman_code litlen_codes[hlit];
+	memset(litlen_codes, 0, sizeof(huffman_code) * hlit);
+	read_code_length_for_litlen(reader, 
+								litlen_codes, hlit, 
+								cl_for_cl, hclen);
+	*/
 }
 
 void build_huffman_codes(huffman_code huff_code[], size_t size)
@@ -36,10 +47,13 @@ void build_huffman_codes(huffman_code huff_code[], size_t size)
 	memset(bl_count, 0, sizeof(size_t) * bl_count_size);
 
 	count_number_of_codes(bl_count, huff_code, size);
+	print_massive(bl_count, bl_count_size);
 
 	size_t max_bits = get_max_bits(bl_count, bl_count_size);
 	size_t next_code[max_bits + 1];
 	find_numerical_value_of_smallest_code(next_code, bl_count,max_bits);
+	printf("next_code:\n");
+	print_massive(next_code, max_bits + 1);
 	assign_numerical_values_to_all_codes(next_code, huff_code, size);
 }
 
@@ -50,7 +64,8 @@ void count_number_of_codes(size_t bl_count[],
 {
 	int i;
 	for (i = 0; i < cl_size; i++)
-		bl_count[huff_code[i].code_len]++;
+		if (huff_code[i].code_len != 0)
+			bl_count[huff_code[i].code_len]++;
 }
 
 void find_numerical_value_of_smallest_code(size_t next_code[], 
@@ -83,7 +98,7 @@ void assign_numerical_values_to_all_codes(size_t next_code[],
 size_t get_max_bits(size_t bl_count[], size_t bl_count_size)
 {
 	size_t i;
-	for (i = bl_count_size - 1; i > 0; i++) {
+	for (i = bl_count_size - 1; i > 0; i--) {
 		if (bl_count[i] > 0)
 			break;
 	}
@@ -94,8 +109,16 @@ void print_huffman_codes(huffman_code huff_code[], size_t size)
 {
 	size_t i;
 	for (i = 0; i < size; i++) {
-		printf("code = ");
+		printf("%d: code = ", i);
 		print_bytes(huff_code[i].code, sizeof(size_t));
 		printf(", code_len = %d\n", huff_code[i].code_len);
+	}
+}
+
+void print_massive(size_t mas[], size_t size)
+{
+	size_t i;
+	for (i = 0; i < size; i++) {
+		printf("%d: %d\n", i, mas[i]);
 	}
 }
