30294915b1b276c93a85c4872f26332636f62a73
Marsel Zaripov <marszaripov@gmail.com>
2013-03-17 14:56:08 +0400

    add writting bytes from cyclic queue

--- static_inflate.c
+++ static_inflate.c
@@ -8,24 +8,27 @@
 
 void static_inflate(reader_t *reader)
 {
-	cyclic_queue *cqbuff = new_cyclic_queue(DICT_SIZE_Q);
+/*	cyclic_queue *cqbuff = new_cyclic_queue(DICT_SIZE_Q);*/
 	io *io_s;
 	init_io(&io_s);
+	byte bytes[LEN_MAX];
 
 	two_bytes litlen;
 	bool is_end_of_block = false;
 	while (!is_end_of_block) {
 		litlen = decode_next_litlen(reader);
 		if (litlen < END_OF_BLOCK)
-			write_bits(io_s, litlen, N);
+			write_byte(io_s, litlen);
 		else if (litlen == END_OF_BLOCK)
 			is_end_of_block = true;
 		else {
 			two_bytes length = decode_length(reader, litlen);
 			two_bytes distance = decode_distance(reader);
-			printf("length = %d, distance = %d\n", length, distance);
+			get_cyclic_queue(io_s->output, bytes, length, distance);
+			write_bytes(io_s, bytes, length);
 		}
 	}
 	
 	write_to_output(io_s, reader->output);
+	delete_io(&io_s);
 }
