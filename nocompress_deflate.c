8c3fb611cb75e41728ae0fca7fd7082c12b0ff35
Marsel Zaripov <marszaripov@gmail.com>
2013-03-18 15:54:14 +0400

    move write_data function to writer module

--- nocompress_deflate.c
+++ nocompress_deflate.c
@@ -1,12 +1,9 @@
 #include "nocompress_deflate.h"
 #include <stdint.h>
 
-#define BUFF_SIZE 1000
-
 /* internal declarations */
 static void write_nocompress_header(io *io_s);
 static void write_length_of_block(io *io_s);
-static void write_data(io *io_s);
 
 /* definitions */
 void nocompress_deflate(io *io_s)
@@ -31,21 +28,8 @@ static void write_nocompress_header(io *io_s)
 
 static void write_length_of_block(io *io_s)
 {
-	uint32_t size = io_s->block_size;
-	fwrite(&size, sizeof(uint32_t), 1, io_s->output_file);
-}
-
-static void write_data(io *io_s)
-{
-	byte buff[BUFF_SIZE];
-	size_t count = 0, last_count, to_read;
-	while (count < io_s->block_size) {
-		if (io_s->block_size - count < BUFF_SIZE)
-			to_read = io_s->block_size - count;
-		else
-			to_read = BUFF_SIZE;
-		last_count = fread(buff, EL_SIZE, to_read, io_s->input);
-		fwrite(buff, EL_SIZE, last_count, io_s->output_file);
-		count += last_count;
-	}
+	two_bytes len = io_s->block_size;
+	two_bytes nlen = ~len;
+	fwrite(&len, sizeof(two_bytes), 1, io_s->output_file);
+	fwrite(&nlen, sizeof(two_bytes), 1, io_s->output_file);
 }
